65e60632a97d36ae1126efb11af591dd9b67c68c
mehrdad.karami <mehrdadallahkarami@gmail.com>
2025-06-27 18:56:59 +0200

    [Refactor] enhance BaseIntegrationTest with Spring Boot test configuration and active profile for integration tests

--- src/test/kotlin/com/movie/rate/integration/BaseIntegrationTest.kt
+++ src/test/kotlin/com/movie/rate/integration/BaseIntegrationTest.kt
@@ -2,10 +2,14 @@ package com.movie.rate.integration
 
 import io.restassured.RestAssured
 import org.junit.jupiter.api.BeforeEach
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.test.context.ActiveProfiles
 import org.testcontainers.junit.jupiter.Testcontainers
 
 @Testcontainers
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 abstract class BaseIntegrationTest {
 
     @LocalServerPort
